d73cd1e9466b25416e27ca0d455ef803c8df3ad6
sabaca <postmaster@codenodes.com>
2011-10-01 20:09:17 +0400

    alpha 0.03

--- perl.js
+++ perl.js
@@ -1,14 +1,16 @@
+// hi. this is an alpha 0.03 (2011-10-01) of mode/perl.js (text/x-perl)
 // this is a part of CodeMirror from sabaca / postmaster@codenodes.com
 CodeMirror.defineMode("perl",function(config,parserConfig){
 
 	function words(str){
 		var obj={},words=str.split(" ");
-		for(var i=0;i<words.length;++i)
+		for(var i=0;i<words.length;i++)
 			obj[words[i]]=true;
 		return obj}
 
 	function tokenSomething(quote,type,eatMore,eatAfter,eatBefore){
 		return function(stream,state){
+			var ql=quote.length;
 			var escaped=false,next,end=false;
 			while((next=stream.next())!=null){
 				if(next==quote&&!escaped){
@@ -32,34 +34,39 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 			return type=="regexp"?"number":type}}
 
 	var indentUnit=config.indentUnit;
-	var keywords=words("bootstrap new base constant continue else elsif for foreach format semctl shmctl shmget semget goto if last our local my next no package parent redo require scalar sub unless until while use const vars ARGV ENV INC SIG getprotobynumber getprotobyname getservbyname gethostbyaddr gethostbyname getservbyport getnetbyaddr getnetbyname getsockname getpeername setpriority getprotoent setprotoent getpriority endprotoent getservent setservent endservent sethostent socketpair getsockopt gethostent endhostent setsockopt setnetent quotemeta localtime prototype getnetent endnetent rewinddir wantarray getpwuid closedir getlogin readlink endgrent getgrgid getgrnam shmwrite shutdown readline endpwent setgrent readpipe formline truncate dbmclose syswrite setpwent getpwnam getgrent getpwent ucfirst sysread setpgrp shmread sysseek sysopen telldir defined opendir connect lcfirst getppid binmode syscall sprintf getpgrp readdir seekdir waitpid reverse unshift symlink dbmopen semget msgrcv rename listen chroot msgsnd shmctl accept unpack exists fileno shmget system unlink printf gmtime msgctl semctl values rindex substr splice length msgget select socket return caller delete alarm ioctl index undef lstat times srand chown fcntl close write umask rmdir study sleep chomp untie print utime mkdir atan2 split crypt flock chmod BEGIN bless unbless chdir semop shift reset link stat chop grep fork dump join open tell pipe exit glob warn each bind sort pack eval push keys getc kill seek sqrt send wait rand tied read time exec recv eof chr int ord exp pos pop sin log abs oct hex tie cos vec END ref map die uc lc do");
-	var blockKeywords=words("if elsif else unless while do sub map BEGIN END PRINT PRINTF DESTROY TIE UNTIE READ");
-	var atoms=words("sub undef or xor and not x eq ne cmp");
+	var blockKeywords=words("if elsif else unless while do sub map BEGIN END PRINT PRINTF DESTROY TIE DESTRUCT INIT RUN CHECK START CONSTRUCT UNTIE GETC");
+	var atoms=words("ENV STDIN INC STDOUT ARGV STDERR ARGVOUT SIG bootstrap given new base constant getpid strict warnings warn getppid setregid argv prctl setgid setgroups getgid getgroups continue else elsif for foreach format semctl shmctl shmget semget my goto if last our local next no package parent redo require scalar sub unless until while use const vars ARGV ENV INC SIG getprotobynumber getprotobyname getservbyname gethostbyaddr gethostbyname getservbyport getnetbyaddr getnetbyname getsockname getpeername setpriority getprotoent setprotoent getpriority endprotoent getservent setservent endservent sethostent socketpair getsockopt gethostent endhostent setsockopt setnetent quotemeta localtime prototype getnetent endnetent rewinddir wantarray getpwuid closedir getlogin readlink endgrent getgrgid getgrnam shmwrite shutdown readline endpwent setgrent readpipe formline truncate dbmclose syswrite setpwent getpwnam getgrent getpwent ucfirst sysread setpgrp shmread sysseek sysopen telldir defined opendir connect lcfirst getppid binmode syscall sprintf getpgrp readdir seekdir waitpid reverse unshift symlink dbmopen semget msgrcv rename listen chroot msgsnd shmctl accept unpack exists fileno shmget system unlink printf gmtime msgctl semctl values rindex substr splice length msgget select socket return caller delete alarm ioctl index undef lstat times srand chown fcntl close write umask rmdir study sleep chomp untie print utime mkdir atan2 split crypt flock chmod BEGIN bless unbless chdir semop shift reset link stat chop grep fork dump join open tell pipe exit glob warn each bind sort pack eval push keys getc kill seek sqrt send wait rand tied read time exec recv eof chr int ord exp pos pop sin log abs oct hex tie cos vec ref map die uc lc do or xor and not x eq ne cmp");
 	var multiLineStrings=true;
 
 	var hooks={
 		"$":function(stream,state){
-			if(stream.eatWhile(/[\w\$]/))
-				return "meta"},
+			if(stream.eatWhile(/[\w\$\[\]]/))
+				return "meta";
+			return false},
 		"@":function(stream,state){
-			if(stream.eatWhile(/[\w\$]/))
-				return "meta"},
+			if(stream.eatWhile(/[\w\$\[\]]/))
+				return "meta";
+			return false},
 		"%":function(stream,state){
 			if(stream.eatWhile(/[\w\$]/))
-				return "meta"},
+				return "meta";
+			return false},
 		"&":function(stream,state){
 			if(stream.eatWhile(/[\w\$]/))
-				return "meta"},
+				return "meta";
+			return false},
 		":":function(stream,state){
 			if(stream.eat(":"))
-				return "meta"},
+				return "meta";
+			return false},
 		"-":function(stream,state){
 			if(stream.eat(">"))
-				return "meta"},
-		".":function(stream,state){
-			if(stream.look()!=".")
-				return "operator"},
+				return "meta";
+			return false},
 		"q":function(stream,state){
+			var c=stream.look(-2);
+			if(c&&/\w/.test(c))
+				return false;
 			if(/[qwx]/.test(stream.look(0))){
 				if(stream.look(1)=="("){
 					stream.eat(/[qwx]/);
@@ -109,11 +116,15 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 				if(/[\^'"!~\/]/.test(stream.look(1))){
 					stream.eat("r");
 					state.tokenize=tokenSomething(stream.eat(/[\^'"!~\/]/),"regexp",/[gosex]/);
-					return state.tokenize(stream,state)}}},
+					return state.tokenize(stream,state)}}
+			return false},
 		"m":function(stream,state){
-			var c=stream.eat(/[(\[{<\^'"!~\/]/);
+			var c=stream.look(-2);
+			if(c&&/\w/.test(c))
+				return false;
+			c=stream.eat(/[(\[{<\^'"!~\/]/);
 			if(!c)
-				return;
+				return false;
 			if(/[\^'"!~\/]/.test(c)){
 				state.tokenize=tokenSomething(c,"regexp",/[gosex]/);
 				return state.tokenize(stream,state)}
@@ -128,11 +139,15 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 				return state.tokenize(stream,state)}
 			if(c=="<"){
 				state.tokenize=tokenSomething(">","regexp",/[gosex]/);
-				return state.tokenize(stream,state)}},
+				return state.tokenize(stream,state)}
+			return false},
 		"s":function(stream,state){
-			var c=stream.eat(/[(\[{<\^'"!~\/]/);
+			var c=stream.look(-2);
+			if(c&&/\w/.test(c))
+				return false;
+			c=stream.eat(/[(\[{<\^'"!~\/]/);
 			if(!c)
-				return;
+				return false;
 			if(/[\^'"!~\/]/.test(c)){
 				state.tokenize=tokenSomething(c,"regexp",/[gosex]/,c);
 				return state.tokenize(stream,state)}
@@ -147,13 +162,85 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 				return state.tokenize(stream,state)}
 			if(c=="<"){
 				state.tokenize=tokenSomething(">","regexp",/[gosex]/,">","<");
-				return state.tokenize(stream,state)}},
+				return state.tokenize(stream,state)}
+			return false},
+		"y":function(stream,state){
+			var c=stream.look(-2);
+			if(c&&/\w/.test(c))
+				return false;
+			c=stream.eat(/[(\[{<\^'"!~\/]/);
+			if(!c)
+				return false;
+			if(/[\^'"!~\/]/.test(c)){
+				state.tokenize=tokenSomething(c,"regexp",/[gosex]/,c);
+				return state.tokenize(stream,state)}
+			if(c=="("){
+				state.tokenize=tokenSomething(")","regexp",/[gosex]/,")","(");
+				return state.tokenize(stream,state)}
+			if(c=="["){
+				state.tokenize=tokenSomething("]","regexp",/[gosex]/,"]","[");
+				return state.tokenize(stream,state)}
+			if(c=="{"){
+				state.tokenize=tokenSomething("}","regexp",/[gosex]/,"}","{");
+				return state.tokenize(stream,state)}
+			if(c=="<"){
+				state.tokenize=tokenSomething(">","regexp",/[gosex]/,">","<");
+				return state.tokenize(stream,state)}
+			return false},
+		"t":function(stream,state){
+			var c=stream.look(-2);
+			if(c&&/\w/.test(c))
+				return false;
+			c=stream.eat("r");
+			if(!c)
+				return false;
+			c=stream.eat(/[(\[{<\^'"!~\/]/);
+			if(!c)
+				return false;
+			if(/[\^'"!~\/]/.test(c)){
+				state.tokenize=tokenSomething(c,"regexp",/[gosex]/,c);
+				return state.tokenize(stream,state)}
+			if(c=="("){
+				state.tokenize=tokenSomething(")","regexp",/[gosex]/,")","(");
+				return state.tokenize(stream,state)}
+			if(c=="["){
+				state.tokenize=tokenSomething("]","regexp",/[gosex]/,"]","[");
+				return state.tokenize(stream,state)}
+			if(c=="{"){
+				state.tokenize=tokenSomething("}","regexp",/[gosex]/,"}","{");
+				return state.tokenize(stream,state)}
+			if(c=="<"){
+				state.tokenize=tokenSomething(">","regexp",/[gosex]/,">","<");
+				return state.tokenize(stream,state)}
+			return false},
 		"/":function(stream,state){
 			state.tokenize=tokenSomething("/","regexp",/[gosex]/);
 			return state.tokenize(stream,state)},
 		"#":function(stream,state){
 			stream.skipToEnd();
-			return "comment"}};
+			return "comment"},
+/*
+		"<":function(stream,state){
+			if(stream.look(0)=="<"&&stream.look(1)){
+				var c=/^<\s*('(\w+)'|(\w+))/.exec(stream.suffix());
+				if(c[1]){
+					stream.eat("<");
+					eval('stream.eatWhile(/[\s'+c[1]+']/)');
+					c=c[2]||c[3];
+					state.tokenize=tokenSomething(c,"string");
+					return state.tokenize(stream,state)}}
+			return false},
+*/
+		"_":function(stream,state){
+			if(stream.pos==1&&stream.suffix(6)=="_END__"){
+				state.tokenize=tokenSomething('\0',"comment");
+				return state.tokenize(stream,state)}
+			return false},
+		"_":function(stream,state){
+			if(stream.pos==1&&stream.suffix(7)=="_DATA__"){
+				state.tokenize=tokenSomething('\0',"string");
+				return state.tokenize(stream,state)}
+			return false}};
 
 	var curPunc;
 
@@ -166,26 +253,55 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 		if(ch=='"'||ch=="'"){
 			state.tokenize=tokenString(ch);
 			return state.tokenize(stream,state)}
-		if(/[\[\]{}\(\),;]/.test(ch)){
+		if(/\w/.test(ch)){
+			var p=stream.pos;
+			if(stream.look(-2)=="{"&&(stream.look(0)=="}"||stream.eatWhile(/\w/)&&stream.look(0)=="}"))
+				return "string";
+			else
+				stream.pos=p}
+		if(/[\[\]{}\(\);]/.test(ch)){
 			curPunc=ch;
-			return null}
+			return false}
 		if(/\d/.test(ch)){
-			if(stream.eatWhile(/[\d_\.]/))
-				return "number"}
-		if(/\./.test(ch)){
-			if(stream.eatWhile(/[\d_]/))
-				return "number"}
-		if(/[+\-*&%=<>!?|\/~\.]/.test(ch)){
+			stream.eatWhile(/[\d_\.]/);
+			return "number"}
+		if(ch=="."&&/[^\.\w]/.test(stream.look(-2))&&stream.eatWhile(/\d/)){
+			return "number"}
+		if(ch=="."){
+			return "operator"}
+		if(/[+\-\^*\$&%@=<>!?|\/~\.]/.test(ch)){
 			stream.eatWhile(ch);
 			return "operator"}
-		stream.eatWhile(/[\w\$]/);
-		var cur=stream.current();
-		if(keywords.propertyIsEnumerable(cur)){
+		if(/[A-Z]/.test(ch)){
+			var l=stream.look(-2);
+			var p=stream.pos;
+			stream.eatWhile(/[A-Z_]/);
+			if(/[\da-z]/.test(stream.look(0))){
+				stream.pos=p}
+			else{
+				var cur=stream.current();
+				if(blockKeywords.propertyIsEnumerable(cur))
+					curPunc="newstatement";
+				if(l!=":"){
+					if(atoms.propertyIsEnumerable(cur))
+						return "atom";
+					else
+						return "keyword"}
+				else
+					return "word"}}
+		if(/[a-zA-Z_]/.test(ch)){
+			var l=stream.look(-2);
+			stream.eatWhile(/\w/);
+			var cur=stream.current();
 			if(blockKeywords.propertyIsEnumerable(cur))
 				curPunc="newstatement";
-			return "keyword"}
-		if(atoms.propertyIsEnumerable(cur))
-			return "atom";
+			if(l!=":"){
+				if(atoms.propertyIsEnumerable(cur))
+					return "atom";
+				else
+					return "keyword"}
+			else
+				return "word"}
 		return "word"}
 
 	function tokenString(quote){
@@ -199,15 +315,6 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 			if(end||!(escaped||multiLineStrings))
 				state.tokenize=tokenBase;
 			return "string"}}
-
-	function tokenComment(stream,state){
-		var maybeEnd=false,ch;
-		while(ch=stream.next()){
-			if(ch=="/"&&maybeEnd){
-				state.tokenize=tokenBase;
-				break}
-			maybeEnd=ch=="*"}
-		return "comment"}
 
 	function Context(indented,column,type,align,prev){
 		this.indented=indented;
@@ -226,6 +333,7 @@ CodeMirror.defineMode("perl",function(config,parserConfig){
 		return state.context=state.context.prev}
 
 	return{
+		multiLineStrings:true,
 		startState:function(basecolumn){
 			return{
 				tokenize:null,
@@ -284,3 +392,15 @@ CodeMirror.defineMIME("text/x-perl", "perl");
 // it's like "peek", but need for look-ahead or look-behind if index < 0
 CodeMirror.StringStream.prototype.look=function(index){
 	return this.string.charAt(this.pos+(index||0))};
+
+CodeMirror.StringStream.prototype.prefix=function(count){
+	return this.string.substr((count?this.pos-count:0),this.pos+1)};
+
+CodeMirror.StringStream.prototype.suffix=function(count){
+	return this.string.substr(this.pos,(count||(this.string.length-this.pos+1)))};
+
+CodeMirror.StringStream.prototype.nsuffix=function(count){
+	var p=this.pos;
+	var l=count||(this.string.length-this.pos+1);
+	this.pos+=l;
+	return this.string.substr(p,l)};
